f9d900c42c876b017691b551688a121a32838bd3
Tor Krill <tor@openproducts.se>
2014-08-27 11:24:52 +0200

    Add test for fetchmail parse
    Added to facilitate debug of fetchmailclass
    
    
    Signed-off-by: Tor Krill <tor@openproducts.se>

--- tests/TestRegex.cpp
+++ tests/TestRegex.cpp
@@ -44,3 +44,36 @@ void TestRegex::Test()
 	CPPUNIT_ASSERT_NO_THROW( m = r.DoMatch("10 print 'hello world';"));
 	CPPUNIT_ASSERT_EQUAL( (size_t) 4, m.size());
 }
+
+static inline string getmatchstring(const string& line, Regex::Match m)
+{
+	return line.substr(m.rm_so, m.rm_eo - m.rm_so);
+}
+
+static void testonestring(Regex& r, const string& s)
+{
+	vector<Regex::Match> m;
+	CPPUNIT_ASSERT_NO_THROW( m = r.DoMatch(s));
+
+	if(0)
+	{
+		cout << m.size()<<endl;
+		int i =0;
+		for( Regex::Match item: m)
+		{
+			cout << item.rm_so<< ":"<<item.rm_eo<< "[" << getmatchstring(s,m[i++])<<"]"<<endl;
+		}
+	}
+}
+
+void TestRegex::TestFetchmail()
+{
+	Regex r("\\s+user\\s+(\\S+)\\s+there with password\\s+(.+)\\s+is\\s+(\\S+)\\s+here\\s+(ssl|)");
+	//Regex r("\\s+user\\s+(\\S+)\\s+there with password\\s+(.+)\\s+is\\s+(\\S+)\\s+here");
+
+	string nossl("\tuser 'myid' there with password 'secret' is 'admin' here smtpaddress localdomain");
+	string withssl("\tuser 'myid' there with password 'secret' is 'admin' here ssl smtpaddress localdomain");
+
+	testonestring( r, nossl );
+	testonestring( r, withssl );
+}
--- tests/TestRegex.h
+++ tests/TestRegex.h
@@ -7,12 +7,14 @@ class TestRegex: public CppUnit::TestFixture
 {
 	CPPUNIT_TEST_SUITE( TestRegex );
 	CPPUNIT_TEST( Test );
+	CPPUNIT_TEST( TestFetchmail );
 	CPPUNIT_TEST_SUITE_END();
 public:
 	void setUp();
 	void tearDown();
 
 	void Test();
+	void TestFetchmail();
 };
 
 #endif /* TESTREGEX_H_ */
